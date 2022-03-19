3efcf517cac1c04fbbcc58dd4711de9943a15060
divyanshu <divyanshuis801@gmail.com>
2022-03-19 11:01:07 +0530

    updated port status

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api',authRoutes);
 
-const PORT = process.env.PORT || "8000";
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT,() =>{
     console.log(`server is running on port ${PORT} `);
